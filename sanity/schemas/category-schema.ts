daa9a6badfbe6d8fbd9530148e10e03e9e9d9566
ecommercystores <ecommercystores@gmail.com>
2024-07-30 12:56:13 +0300

    update schema

---
+++ sanity/schemas/category-schema.ts
@@ -0,0 +1,25 @@
+import { defineField, defineType } from "sanity";
+
+export const category = defineType({
+  name: "category",
+  title: "Categories",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+    }),
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "name" },
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+  ],
+});
